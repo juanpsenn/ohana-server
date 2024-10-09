eefffbdb2d89bd7742b469b205e54a9805720f51
Juan Pablo Senn <32602005+juanpsenn@users.noreply.github.com>
2024-10-09 23:04:39 +0000

    Add filters by category, location, type

--- app/events/selectors/filters.py
+++ app/events/selectors/filters.py
@@ -7,7 +7,8 @@ from app.models import Event
 class EventFilter(django_filters.FilterSet):
     q = django_filters.CharFilter(method="custom_filter")
     cancelled = django_filters.BooleanFilter(method="custom_cancelled")
-
+    filters = django_filters.CharFilter(method="custom_filters")
+    
     class Meta:
         model = Event
         fields = ["q", "owner_id"]
@@ -19,3 +20,14 @@ class EventFilter(django_filters.FilterSet):
 
     def custom_cancelled(self, queryset, name, value):
         return queryset.filter(cancelled_at__isnull=not value)
+
+    def custom_filters(self, queryset, name, value):
+        filters = value.split(",")
+        q_obj = Q()
+
+        for f in filters:
+            q_obj &= (Q(event_type__name__icontains=f) | 
+                    Q(location__street__icontains=f) | 
+                    Q(category__name__icontains=f))
+
+        return queryset.filter(q_obj)
